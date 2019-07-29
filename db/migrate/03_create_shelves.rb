80354a07273d093971a3635c526d335e90249bbf
ayerest <irisiseve@gmail.com>
2019-07-29 16:42:40 -0700

    wrote methods for Book.rb - shelf joins, and shelves

--- db/migrate/03_create_shelves.rb
+++ db/migrate/03_create_shelves.rb
@@ -1,7 +1,7 @@
 class CreateShelves < ActiveRecord::Migration[5.2]
     def change
         create_table :shelves do |t|
-            t.string :category
+            t.string :name
             t.text :description
             t.integer :user_id
             #shoud description be optional (null by default)
