cf202332202a9745df5e39b6dc4e75bf1ffc0250
marshallhelenm <marshallhelenm@gmail.com>
2019-07-29 15:34:47 -0700

    files

---
+++ lib/api_connector.rb
@@ -0,0 +1,8 @@
+
+
+puts "Please enter a search term:"
+search_term = gets.chomp
+
+responses = JSON.parse(RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{search_term}"))
+
+#connector methods here
