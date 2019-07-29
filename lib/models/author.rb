71a91e5222070dba3b1c47aae3408ffb5bf512a7
marshallhelenm <marshallhelenm@gmail.com>
2019-07-29 16:43:09 -0700

    methods added to shelf and author, not done with author ones

--- lib/models/author.rb
+++ lib/models/author.rb
@@ -1,6 +1,19 @@
 class Author < ActiveRecord::Base
     has_many :books
 
+    def number_of_books
+        self.books.length
+    end
+
+    def number_of_shelves_author_appears_on
+        collections = Shelf.all.map do |shelf|
+        shelf.books
+        end #getting an array of shelves where each shelf is an array of books
+        collections.select do |collection|
+            #this is not done
+        end
+    end
+
     #methods to build
     #number of books
     #number of shelves they appear on
--- lib/models/shelf.rb
+++ lib/models/shelf.rb
@@ -2,10 +2,23 @@ class Shelf < ActiveRecord::Base
     has_many :sbjoins
     has_many :books, through: :sbjoins
 
+
     #methods to build:
-    #books on shelf
-    #create/delete shelf
+    #books on shelf - should be inherent to the has_many statement
+    #rename shelf
+    #create/delete shelf - this is just built in
     #create shelf with random books of a genre or author or whatevs (stretch)
     #authors on shelf
+
+    def rename(name)
+        self.name = name
+    end
+
+    def authors
+        authors = self.books.collect do |book|
+            book.author
+        end
+        authors.uniq
+    end
     
 end
