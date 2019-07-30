1d42be322139b37f3b4849b0a1810417ef77c445
ayerest <irisiseve@gmail.com>
2019-07-30 10:39:18 -0700

    api connector method created

--- lib/api_connector.rb
+++ lib/api_connector.rb
@@ -1,8 +1,9 @@
 
 
 # puts "Please enter a search term:"
-# search_term = gets.chomp
+# search_term = gets.chomp -- cli stuff, we don't want to call this every time we are getting data or looking up info from the api
 
-# responses = JSON.parse(RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{search_term}"))
-
-#connector methods here
+def pull_from_api(search_term)
+    string_response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{search_term}")
+    response_hash = JSON.parse(string_response)
+end
---
+++ lib/bookshelfcli.rb
@@ -0,0 +1,8 @@
+def welcome
+    puts "Hello."
+end
+
+def get_search_term
+    puts "Please enter a search term like a book title or author name."
+    gets.chomp
+end
