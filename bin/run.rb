73816e26cdb878e66bc016a4e0a2e3049ad3e62c
ayerest <irisiseve@gmail.com>
2019-07-29 17:27:29 -0700

    added some instances to test in run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,21 @@ require_relative '../config/environment'
 
 test = "test"
 
+author1 = Author.new("Steinbeck")
+author2 = Author.new("Jemisin")
+author3 = Author.new("Test")
+
+user1 = User.new("Trevor")
+user2 = User.new("Mia")
+
+book1 = Book.new(title: "The Stone Sky", genre: "Fantasy")
+book2 = Book.new(title: "East of Eden", genre: "Fiction")
+book3 = Book.new(title: "Testtttt", genre: "Nonfiction")
+
+shelf1 = Shelf.new(name: "Wishlist", description: "A list of all the books I want to read", user_id: user1.id)
+shelf2 = Shelf.new(name: "Finished", description: "A list of all the books I have read", user_id: user2.id)
+
+sbjoin1 = SBJoin.new(shelf_id: shelf1.id, book_id: book1.id)
+
+
 puts test
