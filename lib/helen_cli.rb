93bf50e52257b204314f63b8400cc1d65e13ed6f
marshallhelenm <marshallhelenm@gmail.com>
2019-07-31 10:43:32 -0700

    completed log_in_sign_up method

--- lib/helen_cli.rb
+++ lib/helen_cli.rb
@@ -8,34 +8,72 @@
 #         create instance + assign it to a variable
 #         create wishlist & read books and associate those shelves with the user
 
-def log_in_sign_up
-    puts "Welcome to Bookshelf!
-    1. Log in
-    2. Sign up"
-    action = gets.chomp.to_i
-    if log in
+
+def log_in
+    logged_in = false
+    until logged_in
         "Please enter a username:"
         username = gets.chomp
-        user = User.all search for user by username
-        if user is found
-            "Welcome #{username}!"
+        active_user = User.all.find do |user|
+            user.name == username
+        end #will provide either a user instance, or nil
+        if active_user #if it's truthy, that is
+            logged_in = true
+            puts "Welcome #{username}!"
+            return active_user
         else
-            "I couldn't find a user by that name.
-            1. try again
-            2. sign up"
+            puts <<-TXT 
+            Sorry, I couldn't find a user by that name!\n
+            1. Try again\n
+            2. Create account\n
+            3. Exit program\n
+            TXT
             action = gets.chomp.to_i
+            if action == 2
+                active_user = create_account
+                logged_in = true
+            elsif action == 3
+                puts "Thanks for using Bookshelf!"
+                exit!
+            end
         end
-    else
-        "Please enter a username:"
-        until user is truthy (does that work?)
+    end
+    active_user
+end
+
+
+def create_account
+    active_user = nil
+    "Please enter a username:"
+        until user
             username = gets.chomp
-            user = User.all search to see if user exists already
-            if user is nil or [] or whatever
-                user = User.create(username)
+            active_user = User.all.find do |user|
+                user.name == username
+            end #will return either a user instance or nil
+            if !active_user 
+                active_user = User.create(username)
+                wishlist = Shelf.create("My Wishlist")
+                read = Shelf.create("My Read Books")
+                active_user.shelves << wishlist << read
                 "Welcome, #{username}!"
             else
                 "Oops! That username is taken! Please enter a different username:"
             end
         end
+    active_user
+end
+
+
+
+def log_in_sign_up
+    puts "Welcome to Bookshelf!"
+    puts "1. Log in"
+    puts "2. Sign up"
+    action = gets.chomp.to_i
+    if action == 1
+        active_user = log_in
+    else
+        active_user = create_account
     end
+    active_user
 end
