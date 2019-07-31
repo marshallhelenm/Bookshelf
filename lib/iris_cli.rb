b4c8886160f68a699e419f94ecb19f860ffa46b3
ayerest <irisiseve@gmail.com>
2019-07-31 10:14:59 -0700

    start of the view shelf method - needs to be broken up into helper methods still

--- lib/iris_cli.rb
+++ lib/iris_cli.rb
@@ -32,8 +32,47 @@
     # 4. Exit to main menu
     # get user input
 
-    def view_shelf(user_id)
-        # find the user shelves using user id == self
-        Shelfjoin.all.find_by(user.id == user.id)
+    def view_shelf(user)
+        # user is directly connected to shelves (they belong to the user, so can just use user.shelves)
         # iterate through list of shelves and print with indices (plus 1)
+        #split this section into a separate print shelves method
+        user.shelves.each_with_index do |shelf, index|
+            if shelf == user.shelves[-1]
+                puts "#{index + 1}. #{shelf.name}"
+                puts "#{index + 2}. Exit to Main Menu"
+            else
+                puts "#{index + 1}. #{shelf.name}"
+            end
+        end
+
+        #get user choice
+        user_input = gets.chomp.to_i
+        #check user choice
+        if user_input < user.shelves.length
+            user.shelves.each_with_index do |shelf, index|
+            if user_input == index
+                puts "You have selected #{shelf.name}"
+                puts "What would you like to do?"
+                puts "1. View shelf contents"
+                puts "2. Modify shelf"# (at this point we would be going to a different method) - specify (add/delete shelf)
+                puts "3. Modify contents"# (different method) - specify (add/delete books)
+                puts "4. Exit to main menu"
+                new_user_input = gets.chomp.to_i
+                case new_user_input
+                when 1
+                    #separate method
+                    shelf.books.each_with_index do |book, index|
+                        puts "#{index + 1}. #{book.title}"
+                    end
+                when 2
+                    #separate method
+                when 3
+                    #separate method
+                when 4
+                    #code to run main menu
+                end
+            end
+        else
+            #code to run main menu (after welcome and signup/login screen)
+        end
     end
