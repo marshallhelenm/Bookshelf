7b757c08882a595fe193636852d0cf41eb828f66
marshallhelenm <marshallhelenm@gmail.com>
2019-07-31 12:11:06 -0700

    working on modify contents

--- lib/bookshelfcli.rb
+++ lib/bookshelfcli.rb
@@ -3,3 +3,8 @@ user_input = main_menu_list #list main menu options & get a choice from the user
 main_menu_action(active_user, user_input) #perform the appropriate action
 
 
+#they select view shelves this happens:
+print_shelf_list(active_user)
+shelf_choice = choose_shelf #a method that takes user input
+
+
