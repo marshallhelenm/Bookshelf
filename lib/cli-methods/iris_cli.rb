b377e36af81298f32e5944ad30663b57870ef4ac
ayerest <irisiseve@gmail.com>
2019-07-31 17:35:25 -0700

    pushing

--- lib/cli-methods/iris_cli.rb
+++ lib/cli-methods/iris_cli.rb
@@ -24,7 +24,6 @@
     def create_shelf_macro(active_user)
         shelf_name = ask_user_for_new_shelf
         create_new_shelf(shelf_name, active_user)
-        puts "\n\nYour new shelf #{shelf_name} has been successfully created!"
     end
 
     def ask_user_for_new_shelf
@@ -44,6 +43,7 @@
             active_user.shelves << my_new_shelf
             active_user.save
         end
+        puts "\n\nYour new shelf #{my_new_shelf.name} has been successfully created!"
     end
 
     def add_books_to_new_shelf
