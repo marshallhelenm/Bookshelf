aa9ad024517388ca2e0b9d6882cb33e31ff351f8
ayerest <irisiseve@gmail.com>
2019-08-01 13:19:29 -0700

    reconfigured logic of the found book action menu and action methods

--- lib/cli-methods/search-books.rb
+++ lib/cli-methods/search-books.rb
@@ -3,16 +3,21 @@
 #find book on the book model returns a book instance, pass that into book_action_menu
 def found_book_action_menu(book)
     menu = <<-MENU
-        Would you like to add #{book.title} to a shelf? (y/n)
+        We found #{book.title}. What would you like to do?    
+        1. Would you like to add this book to a shelf?
+        2. View book description
+        3. Previous Menu
     MENU
     puts menu
-    action = STDIN.gets.chomp
+    action = STDIN.gets.chomp.to_i
 end
 
 #found book menu returns an action, pass that and the book instance here
 def found_book_action(book, action, active_user)
-    if action == "y"
+    if action == 1
         shelf = choose_shelf(active_user)
         shelf.add_book_to_shelf(book)
+    else action == 2
+        #call new method to view description
     end
 end
