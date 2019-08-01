ce193bcb50e622a83d03cd746ab62909f01761ca
marshallhelenm <marshallhelenm@gmail.com>
2019-08-01 15:35:22 -0700

    fixing logic for creating a shelf that already exists

--- lib/cli-methods/4-Modify-Shelves.rb
+++ lib/cli-methods/4-Modify-Shelves.rb
@@ -10,12 +10,12 @@ end
 
 def menu_four
     menu = <<-MENU
-        Modify Shelves\n
-        1. Create a Shelf
-        2. Delete a Shelf
-        3. Modify a Shelf
-        4. Menu
-        MENU
+    Modify Shelves\n
+    1. Create a Shelf
+    2. Delete a Shelf
+    3. Modify a Shelf
+    4. Menu
+    MENU
     puts menu
         action = STDIN.gets.chomp.to_i
 end
@@ -23,7 +23,7 @@ end
 def menu_four_perform(action, active_user)
     case action
     when 1 
-        create_shelf_macro(active_user)
+        create_new_shelf(active_user)
     when 2
         shelf_choice = choose_shelf(active_user)
         delete_shelf(shelf_choice, active_user)
@@ -46,19 +46,18 @@ def shelf_options(active_user) #main thing we run under main menu option 4
     until to_menu == true
         action = modify_shelf_menu #returns a user action
         to_menu = modify_shelf(action, active_user)
-        # choose_shelf_menu_option(shelf_choice) # get user action choice and perform action
     end
 end
 
 def modify_shelf_menu #returns a menu action
     menu = <<-TXT 
-        \n\nWhat would you like to do?\n
-        1. View Shelf Contents 
-        2. Remove Book 
-        3. Add Book 
-        4. Rename Shelf 
-        5. Edit Shelf Description
-        6. Return \n
+    \n\nWhat would you like to do?\n
+    1. View Shelf Contents 
+    2. Remove Book 
+    3. Add Book 
+    4. Rename Shelf 
+    5. Edit Shelf Description
+    6. Return \n
     TXT
     #a stretch goal would be to add an option here to move a book to a different shelf
     puts menu
@@ -102,6 +101,8 @@ def modify_shelf(action, active_user) #takes in a shelf instance
         edit_shelf_description(shelf_choice)
     when 6 #return to main menu
         to_menu = true
+    else
+        unknown_command
     end
     to_menu
 end
@@ -145,31 +146,23 @@ def edit_shelf_description(shelf) #takes a shelf instance
 end
 
 #helper method if the user tries to create a shelf that is already in the database
-    def shelf_already_exists(active_user)
+    def shelf_already_exists(active_user, shelf)
         text = <<-TEXT
-                Oops! That shelf already exists!\n
-                What would you like to do?\n
-                1. Select the existing shelf\n
-                2. Try a new shelf name
+\nOops! That shelf already exists!\n
+What would you like to do?\n
+    1. Modify shelf
+    2. Try a new shelf name\n
         TEXT
         puts text
-        action = STDIN.gets.chomp.to_i
-    end
-
-    #take in user input after getting shelf exists error
-    def action_for_shelf_exists(action)
-        if action == 1
-            shelf_menu(shelf_choice) # print options for how user can interact with the shelf
-            choose_shelf_menu_option(shelf_choice) # get user action choice and perform actio
-        else
-            ask_user_for_new_shelf
+        action = 0
+        until action == 1 || action == 2
+            action = STDIN.gets.chomp.to_i
+            if action == 1
+                modification = modify_shelf_menu # print options for how user can interact with the shelf
+                modify_shelf(modification, active_user)
+                return done = true
+            end
         end
-    end
-
-
-    def create_shelf_macro(active_user)
-        shelf_name = ask_user_for_new_shelf
-        create_new_shelf(shelf_name, active_user)
     end
 
     def ask_user_for_new_shelf
@@ -177,19 +170,23 @@ end
         new_shelf_name = STDIN.gets.chomp
     end
 
-    def create_new_shelf(new_shelf_name, active_user)
-        shelf = Shelf.all.find {|shelf| shelf.name == new_shelf_name }
-        if shelf
-            action = shelf_already_exists(active_user)
-            action_for_shelf_exists(action)
-        else
-            puts "Please enter a description for your new bookshelf #{new_shelf_name} or press 'enter' to skip."
-            new_shelf_description = STDIN.gets.chomp
-            my_new_shelf = Shelf.create(name: new_shelf_name, description: new_shelf_description, user_id: active_user.id)
-            active_user.shelves << my_new_shelf
-            active_user.save
+    def create_new_shelf(active_user)
+        done = false
+        until done == true
+            new_shelf_name = ask_user_for_new_shelf
+            shelf = Shelf.all.find {|shelf| shelf.name == new_shelf_name && shelf.user.name == active_user.name}
+            if shelf
+                done = shelf_already_exists(active_user, shelf)
+            else
+                puts "Please enter a description for your new bookshelf #{new_shelf_name} or press 'enter' to skip."
+                new_shelf_description = STDIN.gets.chomp
+                my_new_shelf = Shelf.create(name: new_shelf_name, description: new_shelf_description, user_id: active_user.id)
+                active_user.shelves << my_new_shelf
+                active_user.save
+                puts "\n\nYour new shelf #{my_new_shelf.name} has been successfully created!"
+                done = true
+            end
         end
-        puts "\n\nYour new shelf #{my_new_shelf.name} has been successfully created!"
     end
 
     def add_books_to_new_shelf
